f10aef0fa9263c8719c5ff2eb0db21fc2c841bf2
Thomas Barlow <github@mysterymayhem.co.uk>
2025-06-02 00:49:46 +0100

    Remove unused init.lua code

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,19 +1,8 @@
-
-local variant = Tracker.ActiveVariantUID
-
 -- Items
 require("scripts/items_import")
 
 -- Maps
-if Tracker.ActiveVariantUID == "maps-u" then
-    Tracker:AddMaps("maps/maps-u.json")  
-else
-    Tracker:AddMaps("maps/maps.json")  
-end  
-
-if PopVersion and PopVersion >= "0.23.0" then
-    Tracker:AddLocations("locations/dungeons.json")
-end
+Tracker:AddMaps("maps/maps.json")
 
 -- Layout
 require("scripts/layouts_import")
@@ -22,6 +11,4 @@ require("scripts/layouts_import")
 require("scripts/locations_import")
 
 -- AutoTracking for Poptracker
-if PopVersion and PopVersion >= "0.26.0" then
-    require("scripts/autotracking")
-end
+require("scripts/autotracking")
