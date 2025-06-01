982bc9e878fbc91f8d5b1a4320231d4cda89b3c1
Thomas Barlow <github@mysterymayhem.co.uk>
2025-06-02 00:52:14 +0100

    Inline imports and remove unused import lines and items

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,14 +1,28 @@
 -- Items
-require("scripts/items_import")
+Tracker:AddItems("items/extra_items.json")
+Tracker:AddItems("items/items.json")
 
 -- Maps
 Tracker:AddMaps("maps/maps.json")
 
 -- Layout
-require("scripts/layouts_import")
+Tracker:AddLayouts("layouts/broadcast.json")
+Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/settings_popup.json")
+Tracker:AddLayouts("layouts/tabs.json")
+Tracker:AddLayouts("layouts/tracker.json")
 
 -- Locations
-require("scripts/locations_import")
+Tracker:AddLocations("locations/logic/macros.json")
+Tracker:AddLocations("locations/Cantina.json")
+Tracker:AddLocations("locations/Episode 1.json")
+Tracker:AddLocations("locations/Episode 2.json")
+Tracker:AddLocations("locations/Episode 3.json")
+Tracker:AddLocations("locations/Episode 4.json")
+Tracker:AddLocations("locations/Episode 5.json")
+Tracker:AddLocations("locations/Episode 6.json")
+Tracker:AddLocations("locations/Overworld.json")
+Tracker:AddLocations("locations/LevelRequirementsDisplay.json")
 
 -- AutoTracking for Poptracker
 require("scripts/autotracking")
--- scripts/items_import.lua
+++
@@ -1,6 +0,0 @@
-
-Tracker:AddItems("items/extra_items.json")
-Tracker:AddItems("items/items.json")
-Tracker:AddItems("items/location_items.json")
-Tracker:AddItems("items/labels.json")
-                
