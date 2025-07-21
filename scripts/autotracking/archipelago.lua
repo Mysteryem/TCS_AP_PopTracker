c12b5849407e7c2b0b9aa7fe11e7cbda3ccaf146
Thomas Barlow <github@mysterymayhem.co.uk>
2025-07-21 21:30:13 +0100

    Add visibility rules for 'All Episodes' locations

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -197,6 +197,9 @@ function onClear(slot_data)
     local story_character_locations_enabled = slot_data["enable_story_character_unlock_locations"] == 1
     Tracker:FindObjectForCode("story_character_locations").Active = story_character_locations_enabled
 
+    local all_episodes_purchases_enabled = slot_data["enable_all_episodes_purchases"] == 1
+    Tracker:FindObjectForCode("all_episodes_purchase_locations").Active = all_episodes_purchases_enabled
+
     -- Hint tracking disabled until PopTracker 0.32.0 is released, which adds section highlighting.
 --     if Archipelago.PlayerNumber > -1 then
 --
